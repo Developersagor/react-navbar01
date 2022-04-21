137d401d1d4ba0cff8ff76bfd151f9e08b36d119
sagor <contactme.srbd@gmail.com>
2022-04-21 14:27:07 +0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
-ReactDOM.render(
+import React from "react";
+import "./index.css";
+import App from "./App";
+import reactDom from "react-dom/client";
+const root = reactDom.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
-
-
